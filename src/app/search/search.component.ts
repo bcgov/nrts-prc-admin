2ac04670583c1314ade58c2cfa0ec54ce37ab22c
Severin Beauvais <severin.beauvais@gmail.com>
2018-08-22 09:09:02 -0700

    PRC-574: fixed comment line displaying prematurely

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -91,6 +91,7 @@ export class SearchComponent implements OnInit, OnDestroy {
                         }
                       },
                       error => {
+                        value[0].loaded = true;
                         self.snackBarRef = self.snackBar.open('Error retrieving application ...', null, { duration: 3000 });
                       }
                     );
