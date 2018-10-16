4519b07630136ea86312513ccd59925add4cc5e5
Severin Beauvais <severin.beauvais@gmail.com>
2018-10-16 10:57:34 -0700

    PRC-753: deleted obsolete Add Comment component + updated custom File Upload component (from Public plus changes to look like orig Admin component) + moved stand-alone components to root + cleaned up modules + updated add-edit page to use custom File Upload component + removed references to old file upload + null deref check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+// external imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { BootstrapModalModule } from 'ng2-bootstrap-modal';
 import { CookieService } from 'ngx-cookie-service';
 
-import { AppRoutingModule } from 'app/app-routing.module';
+// modules
 import { SharedModule } from 'app/shared.module';
+import { ApplicationsModule } from 'app/applications/applications.module';
+import { AppRoutingModule } from 'app/app-routing.module';
 
 // components
 import { AppComponent } from 'app/app.component';
@@ -18,6 +21,12 @@ import { HomeComponent } from 'app/home/home.component';
 import { SearchComponent } from 'app/search/search.component';
 import { LoginComponent } from 'app/login/login.component';
 import { ConfirmComponent } from 'app/confirm/confirm.component';
+import { HeaderComponent } from 'app/header/header.component';
+import { FooterComponent } from 'app/footer/footer.component';
+import { AdministrationComponent } from 'app/administration/administration.component';
+import { UsersComponent } from 'app/administration/users/users.component';
+import { AddEditUserComponent } from 'app/administration/users/add-edit-user/add-edit-user.component';
+import { SelectOrganizationComponent } from 'app/select-organization/select-organization.component';
 
 // services
 import { SearchService } from 'app/services/search.service';
@@ -32,16 +41,6 @@ import { DecisionService } from 'app/services/decision.service';
 import { UserService } from 'app/services/user.service';
 import { CanDeactivateGuard } from 'app/services/can-deactivate-guard.service';
 import { ConfigService } from 'app/services/config.service';
-
-// feature modules
-import { ApplicationsModule } from 'app/applications/applications.module';
-import { CommentingModule } from 'app/commenting/commenting.module';
-import { HeaderComponent } from 'app/header/header.component';
-import { FooterComponent } from 'app/footer/footer.component';
-import { AdministrationComponent } from 'app/administration/administration.component';
-import { UsersComponent } from 'app/administration/users/users.component';
-import { AddEditUserComponent } from 'app/administration/users/add-edit-user/add-edit-user.component';
-import { SelectOrganizationComponent } from 'app/applications/select-organization/select-organization.component';
 
 @NgModule({
   declarations: [
@@ -58,13 +57,12 @@ import { SelectOrganizationComponent } from 'app/applications/select-organizatio
     SelectOrganizationComponent
   ],
   imports: [
-    BrowserAnimationsModule,
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     SharedModule,
     ApplicationsModule,
-    CommentingModule,
     AppRoutingModule, // <-- module import order matters - https://angular.io/guide/router#module-import-order-matters
     NgbModule.forRoot(),
     NgxPaginationModule,
@@ -88,7 +86,8 @@ import { SelectOrganizationComponent } from 'app/applications/select-organizatio
   ],
   entryComponents: [
     ConfirmComponent,
-    AddEditUserComponent
+    AddEditUserComponent,
+    SelectOrganizationComponent
   ],
   bootstrap: [AppComponent]
 })
