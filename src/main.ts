08c5f86c352ecc53ae8f9a1912eb2871a61934c2
barrfalk <bfalk@salussystems.com>
2025-03-24 16:04:08 -0700

    keycloak updates

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ if (environment.production) {
 function loadKeycloakScript(): Promise<void> {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/keycloak-js@26.1.4/lib/keycloak.js';
+    script.src = 'https://app.unpkg.com/keycloak-js@25.0.6/files/dist/keycloak.min.js';
     script.onload = () => resolve();
     script.onerror = () => reject('Failed to load Keycloak');
     document.head.appendChild(script);
